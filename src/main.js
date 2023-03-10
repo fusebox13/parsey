14921a21fc1756872d8746def0c6702edb06620a
Daniel Williams <fusebox13@gmail.com>
2023-03-10 00:04:12 -0500

    Update the script to STDOUT the movie list as a CSV

--- src/main.js
+++ src/main.js
@@ -1,23 +1,38 @@
 (async function () {
-    require('dotenv').config();
-    const { Client, GatewayIntentBits, Events } = require('discord.js');
+    require('dotenv').config()
+    const { Client, GatewayIntentBits } = require('discord.js')
 
     const client = new Client({ intents:[
             GatewayIntentBits.Guilds,
             GatewayIntentBits.GuildMessages,
             GatewayIntentBits.MessageContent
         ]
-    });
-
-    client.on(Events.ClientReady, () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
+    })
 
     await client.login(process.env.CLIENT_TOKEN);
 
     const channel = await client.channels.fetch(process.env.CHANNEL_ID)
 
-    let messages = await channel.messages.fetch({limit: 10, after: process.env.MESSAGE_CURSOR})
+    let messages = await channel.messages.fetch({limit: 100, after: process.env.MESSAGE_CURSOR})
 
-    messages.forEach(message => console.log(message.content))
+    let movies = []
+    let messageIds = []
+    let cursor = -1;
+
+    while(messages.size > 0) {
+        messages.forEach(message => {
+            if ( message.embeds && message.embeds[0] && message.embeds[0].title) {
+                movies.push(message.embeds[0].title)
+            }
+            messageIds.push(message.id)
+        })
+        cursor = messageIds.shift();
+        messageIds = [];
+
+        messages = await channel.messages.fetch({limit: 100, after: cursor})
+    }
+    console.log(movies.join('\n').replaceAll(/\([A-Za-z 0-9]+\) - IMDb/g, ""))
+
+    client.destroy()
+
 })();
